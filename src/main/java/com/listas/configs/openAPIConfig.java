c94d4725052ce37a8afae0474fb599a99ccd49e4
Victor Augusto Alves de Souza <victor.souza@gpnet.com.br>
2022-09-17 20:45:47 -0300

    Adicionando crud de lista

---
+++ src/main/java/com/listas/configs/openAPIConfig.java
@@ -0,0 +1,28 @@
+package com.listas.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.tags.Tag;
+
+@Configuration
+public class openAPIConfig {
+
+	@Bean
+	public OpenAPI springShopOpenApi() {
+		OpenAPI openApi = new OpenAPI();
+		
+		Info info = new Info(); 
+		info.title("Avaliação 03 - Victor Augusto Alves de Souza"); 
+		info.version("v0.0.1"); 
+		openApi.info(info);
+		
+		openApi.addTagsItem(new Tag().name("Lista").description("Gerencia as listas do sistema"));
+		
+		info.description("descricao aqui mudar depois"); 
+		
+		return openApi;
+	}
+}
--- src/main/java/com/listas/controllers/ListaController.java
+++ src/main/java/com/listas/controllers/ListaController.java
@@ -1,13 +1,18 @@
 package com.listas.controllers;
 
 import java.net.URISyntaxException;
+import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,7 +24,10 @@ import com.listas.dto.outputs.ListaOutput;
 import com.listas.entities.ListaEntity;
 import com.listas.services.ListaService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "Lista")
 @RestController
 @RequestMapping("/api/listas")
 @CrossOrigin(origins = "*")
@@ -31,6 +39,7 @@ public class ListaController {
 	@Autowired
 	private ListaConvert listaConvert;
 
+	@Operation(summary = "Cadastra lista",description = "Cadastra uma nova lista")
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public ListaOutput criaLista(@Valid @RequestBody ListaInput lista) throws URISyntaxException {
@@ -38,4 +47,46 @@ public class ListaController {
 		ListaEntity listaCriada = listaService.cria(listaEntity);
 		return listaConvert.entityToOutput(listaCriada);
 	}
+	
+	@Operation(summary = "Altera lista")
+	@PutMapping("/{id}")
+	public ListaOutput alteraLista(@PathVariable Long id, @Valid @RequestBody ListaInput listaInput) {
+		ListaEntity listaEntity = listaService.buscaPeloId(id);
+		listaConvert.copyDataInputToEntity(listaInput, listaEntity);
+
+		//convertAutores(listaInput, listaEntity);
+
+		ListaEntity livroAlterado = listaService.atualiza(listaEntity);
+		return listaConvert.entityToOutput(livroAlterado);
+	}
+
+	@Operation(summary = "Busca lista por id")
+	@GetMapping("/{id}")
+	public ListaOutput buscaAutorPorId(@PathVariable Long id) {
+		ListaEntity listaEntity = listaService.buscaPeloId(id);
+		return listaConvert.entityToOutput(listaEntity);
+	}
+	
+	@Operation(summary = "Remove lista")
+	@DeleteMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void removeLista(@PathVariable Long id) {
+		ListaEntity listaEntity = listaService.buscaPeloId(id);
+		listaService.remove(listaEntity);
+	}
+
+	@Operation(summary = "Lista as listas")
+	@GetMapping
+	public List<ListaOutput> listaAutores() {
+		List<ListaEntity> listaTodos = listaService.listaTodos();
+		return listaConvert.entityToOutput(listaTodos);
+	}
+
+	//private void convertAutores(LivroInput livro, LivroEntity livroEntity) {
+		//List<AutorEntity> autores = new ArrayList<>();
+		//for (Long autorId : livro.getAutoresIds()) {
+		//	autores.add(autorService.buscaPeloId(autorId));
+		//}
+		//livroEntity.setAutores(autores);
+	//}
 }
